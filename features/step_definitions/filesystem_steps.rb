d140c7079dc729f33adc73eb247d4b533ebf39de
Aldric Giacomoni <trevoke@gmail.com>
2013-01-31 13:24:52 -0500

    Make sure we properly create symlinks in feature

--- features/step_definitions/filesystem_steps.rb
+++ features/step_definitions/filesystem_steps.rb
@@ -1,3 +1,4 @@
+require 'pathname'
 require 'fileutils'
 require 'tempfile'
 require 'tmpdir'
@@ -18,15 +19,21 @@ end
 And /^"(.*?)" in the home directory should be a "(.*?)"$/ do |file, filetype|
   filepath = File.expand_path File.join(@homedir, file)
   case filetype
-  when "symlink"
-    File.symlink?(filepath).should be_true
-  else
-    raise ArgumentError
+    when 'symlink'
+      File.symlink?(filepath).should be_true
+    else
+      raise ArgumentError
   end
 end
 
 Then /^"(.*?)" in the home directory should point to the file in the config directory$/ do |file|
-  filepath = File.expand_path File.join(@homedir, file)
+  destpath = File.expand_path File.join(@configdir, file)
+  orig_dir = Dir.pwd
+  Dir.chdir @configdir
+  symlink_destination = Pathname.new(file).realpath.to_s
+  Dir.chdir orig_dir
+  symlink_destination.should match Regexp.escape(destpath)
+
 end
 
 After do
@@ -38,5 +45,5 @@ Then /^there should be "(.*?)" files? in the config directory$/ do |number|
   count = number.to_i
   files_in_config = Dir.entries(@configdir)
   # Magic number alert! This includes ., .., and .git
-  files_in_config.size.should eq count + 3 
+  files_in_config.size.should eq count + 3
 end
