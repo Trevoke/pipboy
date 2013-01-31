0bece45fdbd0e785a42b3fc3dcda32268cb2db73
Aldric Giacomoni <trevoke@gmail.com>
2013-01-31 13:21:48 -0500

    Require 'git' in eff_key for consistency

--- lib/pipboy/eff_key.rb
+++ lib/pipboy/eff_key.rb
@@ -1,9 +1,11 @@
+require 'git'
+
 module Pipboy
   class EffKey
     def initialize configdir
-      @g = Git.open configdir
+      @g = ::Git.open configdir
     rescue ArgumentError
-      @g = Git.init configdir
+      @g = ::Git.init configdir
     end
 
     def save file
